f4246607ea6434de9609f7a51d38659e0b301a93
Ibra-Alpha BA <ibbraba@gmail.com>
2023-06-19 17:43:56 +0200

    WebService buisness logic

---
+++ src/main/java/com/doranco/webservices/RESTApplication.java
@@ -0,0 +1,9 @@
+package com.doranco.webservices;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+@ApplicationPath("/api")
+public class RESTApplication extends Application {
+
+}
---
+++ src/main/java/com/doranco/webservices/ressources/CommentaireRessource.java
@@ -0,0 +1,95 @@
+package com.doranco.webservices.ressources;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.doranco.metier.Article;
+import com.doranco.metier.Commentaire;
+
+
+
+
+@WebService (serviceName = "commentaire")
+public class CommentaireRessource {
+
+	
+	@WebMethod
+	@Path("/test")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String testWebService(){
+		return "OK Web Service";
+	}
+	
+	
+	@WebMethod
+	@PathParam("/{articleId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Commentaire> getCommentairesArticle(@PathParam("id") int id) throws SQLException {
+
+		List<Commentaire> listCommentaires = new ArrayList<Commentaire>(); 
+
+		try {
+			// TODO: Import DAO commentaires impl et query commentaires de l'article
+
+		} catch (Exception ex) {
+
+		}
+
+		return listCommentaires;
+
+	}
+	
+	
+	
+	@WebMethod
+	@Produces(MediaType.APPLICATION_JSON)
+	public Object getCommentaires(@PathParam("login") String login, @PathParam("password") String password) {
+
+		HashMap<Article, ArrayList<Commentaire>> listCommentaires = new HashMap<Article, ArrayList<Commentaire>>();
+		
+
+		try {
+			//LOGIN USER 
+			
+			//CHECKER SI USER A LE PROFIL WEBSERVICE COMMENTAIRE
+			
+			//GET ALL ARTICLES 
+			
+			//FOREACH ARTICLE IN ARTICLES 
+			
+			//NEW LIST COMMENTAIRE
+			
+			//GET ARTICLES COMMENTAIRES
+			
+			//listCommentaires.Add(ARTICLE, LIST COMMENTAIRES)
+			
+			// TODO: Import DAO commentaires impl et query tous les commentaires
+
+		} catch (Exception ex) {
+
+		}
+
+		return listCommentaires;
+
+	}
+
+	
+		
+	
+	
+	
+	
+
+	
+
+}
